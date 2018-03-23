a38bd16b5518d989c2e2fb9c8e26de31605bfa9f
AlexBushynskyi <DangerWolfTrendOption@users.noreply.github.com>
2018-03-23 09:02:44 +0200

    fixed bugs

--- script/task7.js
+++ script/task7.js
@@ -2,6 +2,7 @@ function convertToRoman(n) {
 	var font_ar = [1,    4,   5,  9,  10,  40, 50,  90, 100,400, 500,900,1000];
 	var font_rom = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
 	var rezult = "";
+	var text = n+'';
 	var num = font_ar.length - 1;
 	while (text > 0) {
 		if (text >= font_ar[num]) {
